1dc692f483942b58a3e92d048a56770aed2590b6
leader1313 <leader1313@naver.com>
2021-09-09 20:45:24 +0900

    update demo success rate

--- utils.py
+++ utils.py
@@ -1,8 +1,18 @@
 from datetime import datetime, timezone, timedelta
 
 
-def now_stamp():
-    now = datetime.now(timezone(timedelta(hours=9)))
-    yd_string = now.strftime('%Y%m%d')
-    time_string = now.strftime('%H%M%S')
-    return yd_string, time_string
+class ID_generator():
+    def __init__(self):
+        self.now = datetime.now(timezone(timedelta(hours=9)))
+
+    def time_stamp(self):
+        now = datetime.now(timezone(timedelta(hours=9)))
+        return now.strftime('%H%M%S')
+
+    def date_stamp(self):
+        now = datetime.now(timezone(timedelta(hours=9)))
+        return now.strftime('%Y%m%d')
+
+    def now_stamp(self):
+        now = datetime.now(timezone(timedelta(hours=9)))
+        return now.strftime('%Y%m%d%H%M%S')
